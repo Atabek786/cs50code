67aaf76e032fbc1c02f2ab2360d14b8330d20bcc
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-01 15:00:32 +0000

    Thu, Jun 1, 2023, 6:00 PM +03:00

--- lambs/population.c
+++ lambs/population.c
@@ -10,4 +10,11 @@ int main(void)
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
+
+    int l;
+    do
+    {
+        l = get_int ("Number of llams: ");
+    }
+    while (l < 11 || l > 12);
 }
