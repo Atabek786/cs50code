bcc4b7ba1cc2fe3ed54a0fba4af8bd300b148c4a
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-01 12:16:14 +0000

    Thu, Jun 1, 2023, 3:16 PM +03:00

--- mario.c
+++ mario.c
@@ -3,8 +3,13 @@
 
 int main (void)
 {
-    int n = get_int ("Size: ");
-    
+    int n;
+    do
+    {
+        n=get_int("Size: ");
+    }
+    while (n<1);
+
     for (int r=0 ; r<n; r++)
     {
         for (int c=0; c<n; c++)
