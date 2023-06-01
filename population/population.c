00131cfaf05b732f590a1efd42e171c5e1a2f981
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-01 15:50:29 +0000

    Thu, Jun 1, 2023, 6:50 PM +03:00

---
+++ population/population.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+
+    int start;
+    do
+    {
+        start = ()
+    }
+
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
