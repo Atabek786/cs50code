683ffcc6c47f8ef78ba0ab68f7386eb5a29c02c6
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-04 16:42:43 +0000

    Sun, Jun 4, 2023, 7:42 PM +03:00

--- array.c
+++ array.c
@@ -6,8 +6,8 @@ int main (void)
     int scores[3] ;
     for (int i = 0; i <3; i++)
 {
-    scores[i] = get_int ("Type your score: ")
+    scores[i] = get_int ("Type your score: ");
 }
 
-
+    printf ("Your average score is %i", (scores[0] + scores[1] + scores[2] ) / 3);
 }
