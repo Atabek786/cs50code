62c5265cf40987d6260d8a6d4dd40907230ed726
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-08 11:56:23 +0000

    Thu, Jun 8, 2023, 2:56 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,9 +19,25 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf ("Player 1 wins!");
+        return 0;
+    }
+    if else (score1 < score2)
+    {
+        printf ("Player 2 wins!");
+        return 0;
+    }
+    else
+    {
+        printf ("Tie!");
+        return 0;
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    
 }
