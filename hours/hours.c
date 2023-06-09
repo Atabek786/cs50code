fa2200914879c1e232b0ad00c05c20fa9cef8017
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-09 12:42:10 +0000

    Fri, Jun 9, 2023, 3:42 PM +03:00

--- hours/hours.c
+++ hours/hours.c
@@ -27,23 +27,22 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    if (output = 'T')
-    {
+
     int sum = 0;
     for (int i = 0; i < output; i++)
     {
         sum += hours[weeks] ;
         return sum;
     }
-    }
 
-    else if (output = 'A')
-    {
+
+
+
     int avg = 0;
     for (int j = 0; j < output; j++)
     {
         avg = sum / weeks ;
         return avg;
     }
-    }
+    
 }
