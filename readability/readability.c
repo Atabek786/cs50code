b9f80ab45cb8be10a9b25641ebf7f4bfd84c5a7d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-13 21:12:58 +0000

    Wed, Jun 14, 2023, 12:12 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,7 +7,7 @@ int main(void)
 {
     string text = get_string("Text: ");
     int letters = 0;
-    int words = 0;
+    int words = 1;
     int sentences = 0;
 
     for (int i = 0; i <= strlen(text); i++)
@@ -16,8 +16,15 @@ int main(void)
         {
             letters++;
         }
-    }
 
-    for ()
+        else if (text[i] = ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] =='?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
     printf ("%s\n%i letters\n", text, letters);
 }
