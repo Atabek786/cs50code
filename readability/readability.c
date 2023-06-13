e9d9b7242eb6c214db018130d418bf3cb51890f7
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-13 21:07:03 +0000

    Wed, Jun 14, 2023, 12:07 AM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,28 +3,21 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text);
-int count_words(string text);
-int count_sentences(string text);
-
 int main(void)
 {
     string text = get_string("Text: ");
     int letters = 0;
-    printf ("%s\n", text);
+    int words = 0;
+    int sentences = 0;
 
-    letters = count_letters(text);
-}
-int count_letters(string text)
-{
-    int letters = 0;
     for (int i = 0; i <= strlen(text); i++)
     {
-        if (isalpha(text[i]))
+        if(isalpha(text[i]))
         {
             letters++;
         }
-        printf("%i letters\n", letters);
     }
-    return 0;
+
+    for 
+    printf ("%s\n%i letters\n", text, letters);
 }
