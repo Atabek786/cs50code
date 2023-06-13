cf13261cd5660443697cac722f2d258d37394ba2
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-13 20:45:29 +0000

    Tue, Jun 13, 2023, 11:45 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,12 +8,17 @@ int count_letters(string text);
 int main(void)
 {
     string text = get_string("Text: ");
+    
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
     printf ("%s\n", text);
 }
 int count_letters(string text)
 {
     for (int i = 0; i <= strlen(text); i++)
     {
-        
+        if ()
     }
 }
