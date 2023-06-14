ba38d41be748232496e08c851b00d3ae42979620
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-14 17:45:26 +0000

    Wed, Jun 14, 2023, 8:45 PM +03:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -6,11 +6,13 @@ const int BITS_IN_BYTE = 8;
 
 void print_bulb(int bit);
 
+int convert(long long);
+
 int main(void)
 {
     // TODO
     string message = get_string ("Message: ");
-    decimal decimalVal
+    
 
 }
 
@@ -26,4 +28,17 @@ void print_bulb(int bit)
         // Light emoji
         printf("\U0001F7E1");
     }
+}
+
+int convert(long long n) {
+  int dec = 0, i = 0, rem;
+
+  while (n!=0) {
+    rem = n % 10;
+    n /= 10;
+    dec += rem * pow(2, i);
+    ++i;
+  }
+
+  return dec;
 }
