6b7da906ac40c945ded06c0099022d708210331c
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-15 15:00:16 +0000

    Thu, Jun 15, 2023, 6:00 PM +03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,10 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-    if (argc <= 3 )
+    if (argc != 2)
     {
-
+        printf("./caesar key\n");
+        return 1;
     }
+
+    for (int i = 0; i<strlen(argv[1]);i++)
+    {
+        if (!isdigit(argv[1]))
+        {
+            printf("./caesar key\n");
+        }
+    }
+
+    
 }
