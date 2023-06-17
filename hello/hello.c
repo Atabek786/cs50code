3d515342082d83d8ad64e259d3270eec64db9bef
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-17 15:45:59 +0000

    Sat, Jun 17, 2023, 6:45 PM +03:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
-    printf("Hello world\n");
+    string name = get_string("What is your name: "); //declare a string with 'name' variable
+
+    printf("hello, %s\n", name); //then print the name with %s
 }
