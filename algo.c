c83d6e38653ec34d6f67466e676fdd7a8cbe6ca7
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-18 16:57:38 +0000

    Sun, Jun 18, 2023, 7:57 PM +03:00

--- algo.c
+++ algo.c
@@ -21,16 +21,17 @@ int collatz(int n)
         if (n == 1)
             return 1;
         else if (n % 2 == 0)
-        for (int i; i <= 1; i++)
+        for (int i = 0; i <= 1; i++)
         {
             return n/2;
         }
         else
-        for (int j; j <= 1; j++)
+        for (int j = 0; j <= 1; j++)
         {
             return 3 * n + 1;
         }
     }
     while (n > 0);
+    return 0;
 
 }
