f5ec744fa7a203ef832db01ea57084ffc1b19543
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-18 18:23:57 +0000

    Sun, Jun 18, 2023, 9:23 PM +03:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int convert(string input);
 
@@ -26,11 +27,27 @@ int main(void)
 int convert(string input)
 {
     // TODO
-    char string(input);
+    int result = (atoi(input));
+    int num = 0;
+    int i = 0, sign = 1;
 
-    int length = strlen(string);
-    int index = length - 1;
+    while (input == ' ' || input == '\n' || input == '\t')
+    {
+        i++;
+    }
+    if (input == '+' || input == '-')
+    {
+        if(input == '-')
+        {
+            sign = -1;
+        }
+        i++;
+    }
 
-    
-
+    while (input && (input >= '0' && input <= '9'))
+    {
+        num = num * 10 + (input - '0');
+        i++;
+    }
+    return sign * num;
 }
