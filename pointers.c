4f64004696bdba5c3088e75a088e4366e28db0c7
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-20 15:18:48 +0000

    Tue, Jun 20, 2023, 6:18 PM +03:00

--- pointers.c
+++ pointers.c
@@ -11,8 +11,8 @@ int main()
     c = &b;
     *c = 25;
 
-    printf("a has the value %i, located at %p\n", a, &a); //0x123
-    printf("b has the value %i, located at %p\n", b, &b); //50
-    printf("c has the value %i, located at %p\n", c, &c); //25
+    printf("a has the value %i, located at %p\n", a, &a); //14
+    printf("b has the value %i, located at %p\n", b, &b); //25
+    printf("c has the value %i, located at %p\n", c, &c); //0x123
 
 }
