a3b6429afa91cbb479fd497b87b9c9aee806c2e8
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-20 11:00:55 +0000

    Tue, Jun 20, 2023, 2:00 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -165,7 +165,7 @@ void sort_pairs(void)
         {
             if(pairs[j].winner > pairs[j+1].loser)
             {
-                pair tmp = pairs[j];
+                int tmp = pair->pairs[j];
                 pairs[j] = pairs[j+1];
                 pairs[j+1] = tmp;
             }
