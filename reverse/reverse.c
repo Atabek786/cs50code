9d716f0410353f03d83b383610bd9eacb5975524
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-21 17:36:43 +0000

    Wed, Jun 21, 2023, 8:36 PM +03:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -12,13 +12,15 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-    if(argc != 3 && (strcmp(argv[1], "input.wav") != 0 && strcmp(argv[2], "output.wav") != 0))
+    if(argc != 3)
     {
         printf("Usage: ./reverse input.wav output.wav");
         return 1;
     }
+
     // Open input file for reading
     // TODO #2
+    FILE *infile = fopen (argv[1], "r");
 
     // Read header
     // TODO #3
