bc50fe0b567a983e11f89d3c1d0f0a004d77168b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-21 14:53:25 +0000

    Wed, Jun 21, 2023, 5:53 PM +03:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     }
 
     // TODO: Read samples from input file and write updated data to output file
-    
+    for()
     // Close files
     fclose(input);
     fclose(output);
