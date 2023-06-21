74d4742cdd975c617750c9c8ce639b2df6415d80
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-21 19:47:05 +0000

    Wed, Jun 21, 2023, 10:47 PM +03:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -60,7 +60,8 @@ int main(int argc, char *argv[])
     }
     // Write header to file
     // TODO #6
-    
+    WAVHEADER outheader;
+    fwrite(&outheader, sizeof(WAVHEADER), 1, output);
 
     // Use get_block_size to calculate size of block
     // TODO #7
