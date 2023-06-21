ba899902e47d6f3bf492fb9fe5973cb283a5b888
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-21 16:46:22 +0000

    Wed, Jun 21, 2023, 7:46 PM +03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,19 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int row = 0; row <= height; row++)
+    {
+        for (int col = 0; col <= width; col++)
+        {
+            // Calculate grayscale value
+            int grayscalef = (image[row][col].rgbtRed + image[row][col].rgbtGreen + image[row][col].rgbtBlue) / 3;
+
+            // Assign grayscale value to each RGB component
+            image[row][col].rgbtRed = grayscalef;
+            image[row][col].rgbtGreen = grayscalef;
+            image[row][col].rgbtBlue = grayscalef;
+        }
+    }
     return;
 }
 
