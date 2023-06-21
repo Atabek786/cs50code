eeea8b135fe1ec3b857e46a626d8243eb5c078ba
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-21 13:28:07 +0000

    Wed, Jun 21, 2023, 4:28 PM +03:00

--- license/license.c
+++ license/license.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -25,8 +26,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = buffer;
-        idx++;
+        buffer = strcpy(plates[idx]);
     }
 
     for (int i = 0; i < 8; i++)
