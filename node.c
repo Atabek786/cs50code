d4bebb1df98733eeac19d33b59a9ad8d3bba3aad
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-22 16:06:28 +0000

    Thu, Jun 22, 2023, 7:06 PM +03:00

---
+++ node.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cs50.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+}
+node;
+
+#define LIST_SIZE 2
+
+void unload (node *list);
+void visualize (node *list);
+
+int main(void)
+{
+    node *list = NULL;
+
+    for(int i = 0; i < LIST_SIZE; i++)
+    {
+        string phrase = get_string("Enter message: ");
+
+        node *n = malloc(sizeof(node));
+
+        n->phrase = phrase;
+        n->next = NULL;
+        list = n;
+
+        visualize(list);
+    }
+
+    unload(list);
+
+}
