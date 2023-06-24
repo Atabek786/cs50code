fc97f44c296e14c6c94b73f8a5c5b15b4b04c05d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-24 19:06:11 +0000

    Sat, Jun 24, 2023, 10:06 PM +03:00

--- read.py
+++ read.py
@@ -6,5 +6,7 @@ with open("books.csv") as file:
     file_reader = csv.DictReader(file)
     for book in file_reader:
         books.append(book)
+
+
 for book in books:
-    print(book['title'])
+    print(book["title"])
