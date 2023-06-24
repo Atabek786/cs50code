726193a47375d4e4c9754270c21012593818955b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-24 18:51:40 +0000

    Sat, Jun 24, 2023, 9:51 PM +03:00

--- books.py
+++ books.py
@@ -3,7 +3,7 @@ books = []
 # Add three books to your shelf
 for i in range(3):
     book = dict()
-    book["title"] = input("Title: ").strip()
+    book["title"] = input("Title: ").strip().capitalize()
     book["author"] = input("Author: ")
 
 
