fcc2632b18bfce18562dbfc467214802a6af9785
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-24 17:15:52 +0000

    Sat, Jun 24, 2023, 8:15 PM +03:00

---
+++ books.py
@@ -0,0 +1,11 @@
+books = []
+
+# Add three books to your shelf
+for i in range(3):
+    book = dict()
+    book["title"] = input("Title: ")
+    book["author"] = input("Author: ")
+
+
+    books.append(book)
+# Print book titles
