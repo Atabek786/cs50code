936bcc5d30d87bf586b7015d0a09743dbbf32517
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-24 21:15:19 +0000

    Sun, Jun 25, 2023, 12:15 AM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,9 +2,9 @@
 greeting = input("Greeting: ")
 if greeting.isdigit():
     print("Print a string")
-if greeting == str('hello'):
+if greeting == str.isidentifier('hello'):
     print("$0")
-elif greeting == str('h'):
+elif greeting == str.isidentifier('h'):
     print("$20")
 else:
     print("$100")
