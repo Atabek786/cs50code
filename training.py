4d6d40f454b1c49c7c9b918ee0d9ddfe37b62e95
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-24 17:11:29 +0000

    Sat, Jun 24, 2023, 8:11 PM +03:00

--- training.py
+++ training.py
@@ -3,7 +3,15 @@ book["title"] = "Corduroy"
 book["author"] = "Don Freeman"
 print(book["title"])
 
+
+#short version
 book = {
     "title": "Goodnight Moon",
     "author": "Margaret Wise Brown"
 }
+
+
+#multiple dictionaries
+[{"title": "Goodnight Moon", ...},
+ {"title": "Corduroy", ...},
+ {"title": "Curious George", ...}]
