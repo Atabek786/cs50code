d7756ea43ce8f1f74c318e90bf770a23e0ff91ac
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 16:58:05 +0000

    Sun, Jun 25, 2023, 7:58 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -13,4 +13,10 @@ try:
         Figlet.setFont(font = f)
         print(f"Output: {Figlet.renderText(user)}")
     elif sys.argv[1] == '-f' or sys.argv[1] == "--font":
-        if sys.argv[1] == "-f" or sys.argv[1] == 
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            fig = Figlet.setFont(font=sys.argv[2])
+            print(f"Output: {Figlet.renderText(user)}")
+        else:
+            sys.exit("Invalid usage")
+except:
+    sys.exit("Invalid usage")
