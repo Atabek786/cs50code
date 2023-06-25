65482305ce0a99c758a94eaa1e93be68c2171940
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 17:06:29 +0000

    Sun, Jun 25, 2023, 8:06 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,17 +3,18 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 
-user = input("Input: ")
 
 fonts = figlet.getFonts()
 
 try:
     if len(sys.argv) == 1:
+        user = input("Input: ")
         f = random.choice(Figlet.getFonts())
         Figlet.setFont(font = f)
         print(f"Output: {Figlet.renderText(user)}")
     elif sys.argv[1] == '-f' or sys.argv[1] == "--font":
         if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            user = input("Input: ")
             fig = Figlet.setFont(font=sys.argv[2])
             print(f"Output: {Figlet.renderText(user)}")
         else:
