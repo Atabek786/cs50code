ee12c656666da0ec72b2c6c47e249a65846838ed
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 17:39:17 +0000

    Sun, Jun 25, 2023, 8:39 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,25 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
-
+        if capacity < 0:
+            raise ValueError
     def __str__(self):
-        ...
+        n = int(input("Number of cookies: "))
+        print(n)
 
     def deposit(self, n):
-        ...
+        Jar = n + Jar
+        if n > capacity:
+            raise ValueError
 
     def withdraw(self, n):
-        ...
+        Jar = Jar - n
+        if n > Jar:
+            raise ValueError
 
     @property
     def capacity(self):
-        ...
+        return Jar.capacity
 
     @property
     def size(self):
-        ...
+        return Jar
