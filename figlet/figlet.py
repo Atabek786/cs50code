feef84e035620c1a9a17610d9ab8d060a59fab6c
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 16:22:49 +0000

    Sun, Jun 25, 2023, 7:22 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,7 +2,11 @@ from pyfiglet import Figlet
 import pyfiglet
 import sys
 
+program = sys.argv[0]
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
 
+if arg1 == '-f' or arg1 == '--font':
 
 prompt = input("Enter a string: ")
 prompt = Figlet()
