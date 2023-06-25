80db60bf3705b266ea942ba6c86b9d325f16b328
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 16:44:25 +0000

    Sun, Jun 25, 2023, 7:44 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,13 +2,13 @@
 import pyfiglet
 import sys
 
-program = sys.argv[0]
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
+
+
+
 
 
 
 prompt = input("Enter a string: ")
 prompt = pyfiglet.Figlet(prompt)
-
+prompt.setFront(font=f)
 print(pyfiglet.renderText(prompt))
