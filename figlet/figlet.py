47a06aa884ca2543faeb1e45cc236ae2d31486fc
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 17:09:20 +0000

    Sun, Jun 25, 2023, 8:09 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,23 +1,6 @@
-import sys, random
-from pyfiglet import Figlet
+import sys
 
-figlet = Figlet()
-
-
-fonts = figlet.getFonts()
-
-try:
-    if len(sys.argv) == 1:
-        user = input("Input: ")
-        f = random.choice(Figlet.getFonts())
-        Figlet.setFont(font = f)
-        print(f"Output: {Figlet.renderText(user)}")
-    elif sys.argv[1] == '-f' or sys.argv[1] == "--font":
-        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-            user = input("Input: ")
-            fig = Figlet.setFont(font=sys.argv[2])
-            print(f"Output: {Figlet.renderText(user)}")
-        else:
-            sys.exit("Invalid usage")
-except:
-    sys.exit("Invalid usage")
+if len(sys.argv) == 1:
+    isRandomFont = True
+elif len(sys.argv) == 3:
+    
