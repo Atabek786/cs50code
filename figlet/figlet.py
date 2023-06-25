3e37e99f7494c14bec78f5940d4d462d2dab2981
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-06-25 17:11:39 +0000

    Sun, Jun 25, 2023, 8:11 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,5 +2,9 @@ import sys
 
 if len(sys.argv) == 1:
     isRandomFont = True
-elif len(sys.argv) == 3:
-    
+elif len(sys.argv) == 3 and (sys.argv[1] == '-f' or sys.argv[1] == '--font'):
+    isRandomFont = False
+else:
+    sys.exit(1)
+
+print(isRandomFont)
