557c3b2708b6046000811146f7d2afcbfc32a181
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-01 19:54:43 +0000

    Sat, Jul 1, 2023, 10:54 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,19 +1,19 @@
-height = 0
-# Get height input from user and promt until it qualifies
-while(height <= 0 or height > 23):
-    height = int(input("Height: "))
-# Loop to print pyramids on screen
-# Loop to move through rows
-for row in range(height):
-    # Loop to print Spaces
-    for i in range(height - (row + 1)):
-        print(" ", end='')
-    # Loop to print hashes
-    for i in range(row + 1):
-        print("#", end='')
-    # Print 2 spaces for chasim
-    print("  ", end='')
-    # Loop to print other side of pyramid
-    for i in range(row + 1):
-        print("#", end='')
-    print("")
+def main():
+    while True:
+        height = int(input("Height: "))
+        if height >= 1 and height <= 8:
+            break
+
+    for row in range(height):
+        for space in range(height - row - 1):
+            print(" ", end="")
+        for cols in range(row + 1):
+            print("#", end="")
+        print("  ", end="")
+        for cols in range(row + 1):
+            print("#", end="")
+        print()
+
+if __name__ == "__main__":
+    main()
+
