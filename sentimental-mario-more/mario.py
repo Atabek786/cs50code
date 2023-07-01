d8accf2aaca6728c619b6b4f86a98cd6e9c64fb5
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-01 19:43:56 +0000

    Sat, Jul 1, 2023, 10:43 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,8 +2,8 @@ height = int(input("Height: "))
 if height < 1 or height > 8:
     print("give an integer between 1 and 8")
 
-for row in range(height):
-    for space in range(height - row):
+for row in height:
+    for space in (height - row):
         print(' ')
     for cols in range(row + 1):
         print('#')
