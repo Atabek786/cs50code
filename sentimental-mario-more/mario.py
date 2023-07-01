8ff587b8de212e97b8c3f9becac31ac5f3f7880e
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-01 19:29:04 +0000

    Sat, Jul 1, 2023, 10:29 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,8 +5,8 @@ if height < 1 or height > 8:
 for row in range(height):
     for space in range(height - row - 1):
         print(' ')
-    for cols in range(row):
+    for cols in range(row + 1):
         print('#')
     print('  ')
-    for cols in range(row):
+    for cols in range(row + 1):
         print('#')
