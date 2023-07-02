cc4a4c7612806fc5f85ee71a567cc7b68d2293c3
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-02 17:39:42 +0000

    Sun, Jul 2, 2023, 8:39 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,61 +1,36 @@
 
-def main():
-    while True:
-        cents = float(input("Enter the amount in cents: ").strip())
-        if cents > 0:
-            break
+while True:
+    cents = float(input("Enter the amount in cents: ").strip())
+    if cents > 0:
+        break
 
-    cents = round(cents*100)
+cents = round(cents*100)
 
-    count = 0
-    
-    quarters = calculate_quarters(cents)
-    cents = cents - quarters * 25
+count = 0
 
-    dimes = calculate_dimes(cents)
-    cents = cents - dimes * 10
-
-    nickels = calculate_nickels(cents)
-    cents = cents - nickels * 5
-
-    pennies = calculate_pennies(cents)
-    cents = cents - pennies * 1
-
-    coins = quarters + dimes + nickels + pennies
-
-    print("Total number of coins given: " + str(coins))
+while cents >= 25:
+    cents = cents - 25
+    count += 1
 
 
 
+while cents >= 10:
+    cents = cents - 10
+    count += 1
 
-def calculate_quarters(cents):
-    quarters = 0
-    while cents >= 25:
-        cents = cents - 25
-        quarters += 1
-    return quarters
 
-def calculate_dimes(cents):
-    dimes = 0
-    while cents >= 10:
-        cents = cents - 10
-        dimes += 1
-    return dimes
 
-def calculate_nickels(cents):
-    nickels = 0
-    while cents >= 5:
-        cents = cents - 5
-        nickels += 1
-    return nickels
+while cents >= 5:
+    cents = cents - 5
+    count += 1
 
-def calculate_pennies(cents):
-    pennies = 0
-    while cents >= 1:
-        cents = cents - 1
-        pennies += 1
-    return pennies
 
-if __name__=="__main__":
-    main()
+
+while cents >= 1:
+    cents = cents - 1
+    count += 1
+
+
+
+print("Total number of coins given: " + str(count))
 
