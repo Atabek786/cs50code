c33be7d30a1edbaac14106baeb36c470be8c7d00
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-05 15:55:43 +0000

    Wed, Jul 5, 2023, 6:55 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,7 @@
 SELECT description FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = 'Humphrey Street';
+-- Look into transcript of the interviews of witnesses
+SELECT transcript FROM interviews
+WHERE month = 7 AND day = 28
+AND transcript LIKE "%bakery%";
