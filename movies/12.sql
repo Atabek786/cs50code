bd0a8757623694bd571917ad05f45bc916d8f3fe
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-05 10:14:16 +0000

    Wed, Jul 5, 2023, 1:14 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,8 +2,9 @@ SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Johnny Depp"
-AND title IN
-(SELECT title FROM movies
+AND title IN (
+SELECT title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Helena Bohnam Carter");
+WHERE people.name = "Helena Bohnam Carter"
+);
