8e6d35a0dcadfe505a5798d525c1f7e64007f8b6
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-05 09:18:23 +0000

    Wed, Jul 5, 2023, 12:18 PM +03:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,4 @@
-SELECT name FROM people JOIN stars ON people.id = stars.person_id WHERE movie_id = 114709;
+SELECT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movie_id = 114709;
