cc96fa0a502389a3befeb56c28d56c3b8ba38cd4
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-06 18:28:44 +0000

    Thu, Jul 6, 2023, 9:28 PM +03:00

---
+++ trivia/index.js
@@ -0,0 +1,8 @@
+<script>
+    const incbtn = document.querySelectorAll("#incbtn1, #incbtn2, #incbtn3");
+
+    incbtn.addEventListener('click', function onClick() {
+        incbtn.style.backgroundColor = 'salmon';
+        incbtn.style.color = 'white';
+    });
+</script>
