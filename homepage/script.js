e4d29937ccdfc59a75af503f7747e2d156669fae
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-07 17:45:54 +0000

    Fri, Jul 7, 2023, 8:45 PM +03:00

--- homepage/script.js
+++ homepage/script.js
@@ -1,6 +1,6 @@
 // Function to smooth scroll to sections when clicking on the navbar links
 document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll("#links a");
+    const navLinks = document.querySelectorAll("#AboutMe");
     navLinks.forEach(function(link) {
         link.addEventListener("click", function(e) {
             e.preventDefault();
