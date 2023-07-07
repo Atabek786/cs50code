537a6fffae50e0a7c021c6614d99d2d4336a4c3a
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-07 19:25:12 +0000

    Fri, Jul 7, 2023, 10:25 PM +03:00

--- homepage/script.js
+++ homepage/script.js
@@ -2,30 +2,23 @@
 window.onload = function() {
     var welcomeMessage = "Welcome to my weblog!";
     alert(welcomeMessage);
-  };
-
-  // Add a click event listener to a button
-  var button = document.getElementById("myButton");
-  button.addEventListener("click", function() {
-    // Perform an action when the button is clicked
-    console.log("Button clicked!");
-  });
+};
 
   // Fetch data from an API and display it on the page
-  fetch("https://api.example.com/posts")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      // Process the data and display it on the page
-      var postsContainer = document.getElementById("postsContainer");
+fetch("https://api.example.com/posts")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    // Process the data and display it on the page
+    var postsContainer = document.getElementById("postsContainer");
 
-      data.forEach(function(post) {
-        var postElement = document.createElement("div");
-        postElement.innerText = post.title;
-        postsContainer.appendChild(postElement);
-      });
-    })
-    .catch(function(error) {
-      console.log("An error occurred:", error);
+    data.forEach(function(post) {
+    var postElement = document.createElement("div");
+    postElement.innerText = post.title;
+    postsContainer.appendChild(postElement);
     });
+})
+.catch(function(error) {
+    console.log("An error occurred:", error);
+});
