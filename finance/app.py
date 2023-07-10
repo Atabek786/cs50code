b248f50c1a078b15f1ab997bc87bf6da88afb94b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-10 19:13:14 +0000

    Mon, Jul 10, 2023, 10:13 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -44,11 +44,31 @@ def index():
 def buy():
     """Buy shares of stock"""
     if request.method == "POST":
-        symbol = request.form.get("symbol")
-        shares = lookup(symbol)
-        if not shares:
-            return apology("invalid symbol", 400)
-        return render_template("buy.html", )
+        symbol = request.form.get("symbol").upper()
+        shares = request.form.get("shares")
+        if not symbol:
+            return apology("must provide symbol")
+        elif not shares or not shares.isdigit() or int(shares) <= 0:
+            return apology("must provide a positive integer number of shares")
+
+        quote = lookup(symbol)
+        if quote is None:
+            return apology("symbol not found")
+
+        price = quote["price"]
+        total_cost = int(shares) * price
+        cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+
+        if cash < total_cost:
+            return apology("not enough cash")
+
+        db.execute("UPDATE users GET cash = cash - :total_cost WHERE id = :user_id", total_cost=total_cost, user_id=session["user_id"])
+
+        db.execute("INSERT INTO transaction (user_id, symbol, shares, price) VALUES (:user_id, :symbol, :shares, :price)", user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+
+        flash(f"Bought {shares} shares of {symbol} for {usd{total_cost}}!")
+        return redirect("/")
+    
     else:
         return render_template("buy.html")
 
