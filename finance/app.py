4d2644ce1c508c8215a1881a677c886f4f476432
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-10 16:14:06 +0000

    Mon, Jul 10, 2023, 7:14 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -133,7 +133,15 @@ def register():
 
             # Continue with user registration logic here
             # Insert the new user into the users table, handle password hashing, etc.
-            
+            password_hash = db.Column(db.String(128))
+            @property
+            def password(self):
+                raise AttributeError('password is not a readable attribute!')
+            @password.setter
+            def password(self, password):
+                self.password_hash = generate_password_hash(password)
+            def verify_password(self, password):
+                return check_password_hash(self.password_hash, password)
             db.execute("INSERT INTO users (username, hash) VALUES(?, ?)",username, hash)
 
             return "User registration successful"  # Replace this with your desired response
