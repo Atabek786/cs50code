2a2f5c7e053abb7364d344e78428a680489475d1
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-17 15:33:21 +0000

    Mon, Jul 17, 2023, 6:33 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,12 +1,12 @@
 def main():
     time = input("What time is it? ")
-    converted =convert(time)
+    converted = convert(time)
 
-    if 7.0 < time < 8.0:
+    if 7.0 <= converted <= 8.0:
         print("breakfast time")
-    elif 12.0 < time < 13.0:
+    elif 12.0 <= converted <= 13.0:
         print("lunch time")
-    elif 18.0 < time < 19.0:
+    elif 18.0 <= converted <= 19.0:
         print("dinner time")
     else:
         print("")
