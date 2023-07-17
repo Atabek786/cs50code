ee09db1f2103ac9f0a475bf728a104738fac9552
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-17 14:30:45 +0000

    Mon, Jul 17, 2023, 5:30 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,4 +1,15 @@
 prompt = input("File name: ")
 prompt = prompt.lstrip().lower()
 
-if prompt.endswith(.)
+if prompt.endswith(".gif"):
+    print("image/gif")
+elif prompt.endswith(".jpg" or ".jpeg"):
+    print("image/jpeg")
+elif prompt.endswith(".png"):
+    print("image/png")
+elif prompt.endswith(".pdf"):
+    print("image/pdf")
+elif prompt.endswith(".txt"):
+    print("text/txt")
+elif prompt.endswith("..zip"):
+    print("text/zip")
