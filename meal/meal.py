3bcc0124b1f896bfaab3cb906821e1aacc6ec84b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-17 15:30:32 +0000

    Mon, Jul 17, 2023, 6:30 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,19 @@
 def main():
-    ...
+    time = input("What time is it? ")
+    convert(time)
 
+    if 7.0 < time < 8.0:
+        print("breakfast time")
+    elif 12.0 < time < 13.0:
+        print("lunch time")
+    elif 18.0 < time < 19.0:
+        print("dinner time")
+    else:
+        print("")
 
 def convert(time):
-    ...
-
+    float_time = float(time)
+    return float_time
 
 if __name__ == "__main__":
     main()
