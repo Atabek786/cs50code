59bc90f11d274eaef1a08ec07cfaa84b1b8a41f0
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-18 12:45:07 +0000

    Tue, Jul 18, 2023, 3:45 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,6 +5,8 @@ while price > 0:
     insert = int(input("Insert coin: "))
     if insert in [25, 10, 5]:
         price -= insert
+    else:
+        print("only 25, 10, 5 cents are accepted")
 
 
 changed_owed = abs(price)
