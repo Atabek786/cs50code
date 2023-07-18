7cdf1d02bc4de8da31e94fb2e0e918294f59df7e
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-18 15:48:45 +0000

    Tue, Jul 18, 2023, 6:48 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -13,14 +13,46 @@ def is_valid(s):
         return False
     elif len(s) > 6:
         return False
-    ...
+    elif number_check(s) == False:
+        return False
+    elif zero_check(s) == False:
+        return False
+    elif contains_punctuation(s) == True:
+        return False
+    elif doesnt_begin_with_letters(s) == True:
+        return False
+    else:
+        return True
+
 
 def number_check(n):
-    ...
+    firstnum = None
+    for c in n:
+        if c.isdigit():
+            firstnum = c
+            break
+    if firstnum == None:
+        return True
+    if int(firstnum) == 0:
+        return False
+    index = n.index(firstnum)
+    position = len(n) - index
+    for c in n[-position:]:
+        if not c.isdigit():
+            return False
+    return True
+
 
 
 def zero_check(z):
-    ...
+    i = 0
+    while i < len(z):
+        if z[i].isalpha()== False:
+            if z[i] == "0":
+                return False
+            else:
+                break
+        i += 1
 
 def doesnt_begin_with_letters(a):
     if a[0].isalpha() == False or a[1].isalpha() == False:
