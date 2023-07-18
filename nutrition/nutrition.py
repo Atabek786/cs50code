1ca2b28a8e14d4422b5ad3da25db2f2129ff472d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-18 15:58:06 +0000

    Tue, Jul 18, 2023, 6:58 PM +03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,4 +1,63 @@
 products = {
-    ["key:apple", "calories:130"]
-    
+    "apple": {
+        "calories": 130
+    }
+    "avocado": {
+        "calories": 50
+    }
+    "banana": {
+        "calories": 110
+    }
+    "cantaloupe": {
+        "calories": 50
+    }
+    "grapefruit": {
+        "calories": 60
+    }
+    "grapes": {
+        "calories": 90
+    }
+    "honeydew melon": {
+        "calories": 50
+    }
+    "kiwifruit": {
+        "calories": 90
+    }
+    "lemon": {
+        "calories": 15
+    }
+    "lime": {
+        "calories": 20
+    }
+    "nectarine": {
+        "calories": 60
+    }
+    "orange": {
+        "calories": 80
+    }
+    "peach": {
+        "calories": 60
+    }
+    "pear": {
+        "calories": 100
+    }
+    "pineapple": {
+        "calories": 50
+    }
+    "plums": {
+        "calories": 70
+    }
+    "strawberries": {
+        "calories": 50
+    }
+    "sweet cherries": {
+        "calories": 100
+    }
+    "tangerine": {
+        "calories": 50
+    }
+    "watermelon": {
+        "calories": 80
+    }
 }
+
