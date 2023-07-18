b30b9d2b02cad7a759326ee403f5f1663e7a98f4
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-18 12:40:55 +0000

    Tue, Jul 18, 2023, 3:40 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,11 +1,12 @@
 price = 50
-print("Amount Due: ", price)
+
 while price > 0:
+    print("Amount Due: ", price)
     insert = int(input("Insert coin: "))
-    if insert == 25 or insert == 10 or insert == 5:
-        new_price = price - insert
-        print("Amount due: ", new_price)
-    else:
-        print("only 25, 10, 5 cents accepted")
+    if insert in [25, 10, 5]:
+        price -= insert
+        print(price)
 
+changed_owed = abs(price)
 
+print(price)
