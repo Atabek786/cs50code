902ad464695be66f0c5dea9ed6a0f6a780b262b8
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:10:55 +0000

    Wed, Jul 19, 2023, 6:10 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,12 +1,21 @@
 
 def main():
-    fraction = int(input("Fraction: "))
-    
 
 
 
 
 
+
+def get_str(prompt):
+    while True:
+        try:
+            x = input(promt)
+            if x == '1/4':
+                print("25%")
+            elif x == '1/2':
+                print("50%")
+            elif x== '3/4':
+                print("75%")
 
 
 
