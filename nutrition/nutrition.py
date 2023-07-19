a3db6e8b7ce6cbd3957bf6c4a6ffe13c287c7150
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:04:12 +0000

    Wed, Jul 19, 2023, 6:04 PM +03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,68 +1,69 @@
 products = {
     "apple": {
         "calories": 130
-    }
+    },
     "avocado": {
         "calories": 50
-    }
+    },
     "banana": {
         "calories": 110
-    }
+    },
     "cantaloupe": {
         "calories": 50
-    }
+    },
     "grapefruit": {
         "calories": 60
-    }
+    },
     "grapes": {
         "calories": 90
-    }
+    },
     "honeydew melon": {
         "calories": 50
-    }
+    },
     "kiwifruit": {
         "calories": 90
-    }
+    },
     "lemon": {
         "calories": 15
-    }
+    },
     "lime": {
         "calories": 20
-    }
+    },
     "nectarine": {
         "calories": 60
-    }
+    },
     "orange": {
         "calories": 80
-    }
+    },
     "peach": {
         "calories": 60
-    }
+    },
     "pear": {
         "calories": 100
-    }
+    },
     "pineapple": {
         "calories": 50
-    }
+    },
     "plums": {
         "calories": 70
-    }
+    },
     "strawberries": {
         "calories": 50
-    }
+    },
     "sweet cherries": {
         "calories": 100
-    }
+    },
     "tangerine": {
         "calories": 50
-    }
+    },
     "watermelon": {
         "calories": 80
     }
 }
 
 item = input("Item: ")
-if item in products:
-    print("Calories: ", products[item["calories"]])
+
+if item.lower() in products:
+    print("Calories:", products[item.lower()]["calories"])
 else:
-    print("Item not found in store")
+    print("")
