b6b3cbb959ef4e0656404608c2c8a3a0f85cbe20
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:19:42 +0000

    Wed, Jul 19, 2023, 6:19 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,28 +1,38 @@
 def main():
     fuel = get_fraction("Fraction: ")
-    print(f"{fuel}")
+    print(fuel)
 
 
 def get_fraction(prompt):
+    valid_fractions = {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        '4/4': 'F',
+        '0/4': 'E',
+    }
+
     while True:
+        x = input(prompt)
         try:
-            x = input(prompt)
-            if x == '1/4':
-                return "25%"
-            elif x == '1/2':
-                return "50%"
-            elif x == '3/4':
-                return "75%"
-            elif x == '4/4':
-                return "F"
-            elif x == '0/4':
-                return "E"
+            num, denom = x.split('/')
+            num = int(num)
+            denom = int(denom)
+
+            if denom == 0:
+                print("Invalid input. Denominator cannot be zero.")
+            elif num > denom:
+                print("Invalid input. Numerator cannot be greater than the denominator.")
             else:
-                print("Invalid input. Please enter a valid fraction (e.g., '1/4', '1/2', '3/4', '4/4', '0/4')")
-        except ZeroDivisionError:
-            pass
+                fraction = f"{num}/{denom}"
+                if fraction in valid_fractions:
+                    return valid_fractions[fraction]
+                else:
+                    print("Invalid input. Please enter a valid fraction (e.g., '1/4', '1/2', '3/4', '4/4', '0/4')")
         except ValueError:
-            pass
+            print("Invalid input. Please enter a valid fraction (e.g., '1/4', '1/2', '3/4', '4/4', '0/4')")
+        except ZeroDivisionError:
+            print("Invalid input. Denominator cannot be zero.")
 
 
 if __name__ == "__main__":
