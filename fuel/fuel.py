bf90eb4783c06748cc002cf107e452f074de1a9d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:22:36 +0000

    Wed, Jul 19, 2023, 6:22 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,12 @@
 def main():
-    fraction = get_fraction("Enter the fraction (e.g., X/Y): ")
+    # Prompt the user to enter a fraction in the format X/Y
+    fraction = get_fraction("Enter the fraction (e.g., X/Y):")
+
+    # Calculate the percentage of fuel based on the fraction and display the result
     percentage = calculate_percentage(fraction)
 
+    # Check if the tank is essentially empty (1% or less) or essentially full (99% or more),
+    # and output 'E' or 'F' accordingly. Otherwise, print the calculated percentage.
     if percentage <= 1:
         print("E")
     elif percentage >= 99:
@@ -11,24 +16,30 @@ def main():
 
 def get_fraction(prompt):
     while True:
+        # Get the user input for the fraction
         x = input(prompt)
         try:
+            # Split the input into numerator and denominator parts
             num, denom = x.split('/')
             num = int(num)
             denom = int(denom)
 
+            # Validate the fraction: denominator cannot be zero, and numerator should be less than or equal to the denominator
             if denom == 0:
                 print("Invalid input. Denominator cannot be zero.")
             elif num > denom:
                 print("Invalid input. Numerator cannot be greater than the denominator.")
             else:
-                return num, denom
+                return num, denom  # Return the valid fraction as integers
         except ValueError:
             print("Invalid input. Please enter a valid fraction (e.g., 'X/Y')")
 
 def calculate_percentage(fraction):
+    # Calculate the percentage of fuel by dividing the numerator by the denominator and multiplying by 100
     num, denom = fraction
     percentage = (num / denom) * 100
+
+    # Round the percentage to the nearest integer and return the result
     return round(percentage)
 
 if __name__ == "__main__":
