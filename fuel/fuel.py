2c3f0619fee56787633067896d4e4f831a5afa0d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:18:11 +0000

    Wed, Jul 19, 2023, 6:18 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,27 +1,24 @@
-
 def main():
-    fuel = get_str("Fraction: ")
-
+    fuel = get_fraction("Fraction: ")
     print(f"{fuel}")
 
 
-
-
-
-def get_int(prompt):
+def get_fraction(prompt):
     while True:
         try:
-            x = int(input(prompt))
+            x = input(prompt)
             if x == '1/4':
-                print("25%")
+                return "25%"
             elif x == '1/2':
-                print("50%")
-            elif x== '3/4':
-                print("75%")
+                return "50%"
+            elif x == '3/4':
+                return "75%"
             elif x == '4/4':
-                print("F")
+                return "F"
             elif x == '0/4':
-                print("E")
+                return "E"
+            else:
+                print("Invalid input. Please enter a valid fraction (e.g., '1/4', '1/2', '3/4', '4/4', '0/4')")
         except ZeroDivisionError:
             pass
         except ValueError:
