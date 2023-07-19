1e6a43233e3c22fa24d592d1bdd0a933858b7a33
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-19 15:17:38 +0000

    Wed, Jul 19, 2023, 6:17 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -12,15 +12,15 @@ def get_int(prompt):
     while True:
         try:
             x = int(input(prompt))
-            if x == 1/4:
+            if x == '1/4':
                 print("25%")
-            elif x == 1/2:
+            elif x == '1/2':
                 print("50%")
-            elif x== 3/4:
+            elif x== '3/4':
                 print("75%")
-            elif x == 4/4:
+            elif x == '4/4':
                 print("F")
-            elif x == 0/4:
+            elif x == '0/4':
                 print("E")
         except ZeroDivisionError:
             pass
