341c741497446ed7860d565241b3471f5f2c50ba
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 11:38:47 +0000

    Sat, Jul 22, 2023, 2:38 PM +03:00

--- figletpy/figlet.py
+++ figletpy/figlet.py
@@ -6,4 +6,10 @@ import random
 prompt = input("Input: ")
 
 if sys.argv == 0:
-    
+    converted = random(Figlet(prompt))
+    print(converted)
+elif sys.argv == 2:
+    converted = Figlet(sys.argv[2])
+    print(converted)
+else:
+    print("Type 0 or 2 command line arguments")
