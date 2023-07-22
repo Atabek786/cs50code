7b8924ca8a20fab1155ee9a00fe3e22c825a056c
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 21:15:02 +0000

    Sun, Jul 23, 2023, 12:15 AM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -7,26 +7,24 @@ url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 if len(sys.argv) < 2:
     print("Missing command-line argument")
 else:
-    if not sys.argv[1].isdigit():
-        print("Command-line argument is not a number")
-        sys.exit()
-    else:
-        try:
-            response = requests.get(url)
+    try:
+        response = requests.get(url)
 
-            if response.status_code == 200:
-                data = response.json()
-                # The JSON response has a 'bpi' key that contains the price data in different currencies
-                price_usd = float(data['bpi']['USD']['rate'].replace(',',''))
+        if response.status_code == 200:
+            data = response.json()
+            # The JSON response has a 'bpi' key that contains the price data in different currencies
+            price_usd = float(data['bpi']['USD']['rate'].replace(',',''))
 
-                inbitcoin = float(sys.argv[1]) * price_usd
+            inbitcoin = float(sys.argv[1]) * price_usd
 
-                locale.setlocale(locale.LC_ALL, '')
+            locale.setlocale(locale.LC_ALL, '')
 
-                formated = locale.format_string("%.2f", inbitcoin, grouping=True)
-                print(f"${formated}")
-            else:
-                print(f"Request failed with status code: {response.status_code}")
+            formated = locale.format_string("%.2f", inbitcoin, grouping=True)
+            print(f"${formated}")
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+    except ValueError:
+        print("Enter a number of bitcoins")
 
-        except requests.exceptions.RequestException as e:
-            print(f"Request error: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
