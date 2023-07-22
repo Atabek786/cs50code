86e20b5ddc04e07915aacd5e3ef2bce0900ab671
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 12:40:45 +0000

    Sat, Jul 22, 2023, 3:40 PM +03:00

--- game/game.py
+++ game/game.py
@@ -1,2 +1,18 @@
 import random
 
+randomizer = random.randint(1, 9)
+
+try:
+    while True:
+        prompt = int(input("Level: "))
+        if prompt > randomizer:
+            print("Too large")
+            pass
+        elif prompt < randomizer:
+            print("Too small")
+            pass
+        elif prompt == randomizer:
+            print("Just right")
+        else:
+            pass
+        
