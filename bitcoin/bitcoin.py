3f3f1b33e4949e18f4f7070e2cc0ee2525c7e0cb
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 21:32:35 +0000

    Sun, Jul 23, 2023, 12:32 AM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,42 +5,6 @@ import locale
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 
-json = {
-    {
-        "time": {
-            "updated": "Jul 22, 2023 21:28:00 UTC",
-            "updatedISO": "2023-07-22T21:28:00+00:00",
-            "updateduk": "Jul 22, 2023 at 22:28 BST",
-        },
-        "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
-        "chartName": "Bitcoin",
-        "bpi": {
-            "USD": {
-                "code": "USD",
-                "symbol": "&#36;",
-                "rate": "29,832.0354",
-                "description": "United States Dollar",
-                "rate_float": 29832.0354,
-            },
-            "GBP": {
-                "code": "GBP",
-                "symbol": "&pound;",
-                "rate": "24,927.4101",
-                "description": "British Pound Sterling",
-                "rate_float": 24927.4101,
-            },
-            "EUR": {
-                "code": "EUR",
-                "symbol": "&euro;",
-                "rate": "29,060.7580",
-                "description": "Euro",
-                "rate_float": 29060.758,
-            },
-        },
-    }
-}
-
-
 def format_currency(amount):
     # Set the locale to use a comma as the thousands separator
     locale.setlocale(locale.LC_ALL, "")
