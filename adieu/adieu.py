3bdf10602c1621bcc23aedfc49e06aabe175828f
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 12:33:42 +0000

    Sat, Jul 22, 2023, 3:33 PM +03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -14,11 +14,11 @@ except EOFError:
 
 # Create a grammatically correct farewell message using the inflect library
 if len(names) == 1:
-    farewell_message = f"Adieu, adieu to {names[0]}."
+    farewell_message = f"Adieu, adieu, to {names[0]}."
 elif len(names) == 2:
-    farewell_message = f"Adieu, adieu to {names[0]} and {names[1]}."
+    farewell_message = f"Adieu, adieu, to {names[0]} and {names[1]}."
 else:
-    farewell_message = f"Adieu, adieu to {', '.join(names[:-1])}, and {names[-1]}."
+    farewell_message = f"Adieu, adieu, to {', '.join(names[:-1])}, and {names[-1]}."
 
 print(" ".join(farewell_message.split()))
 
