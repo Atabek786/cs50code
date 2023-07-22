16cef3a0e64acd31137d1242ba0f6fafadf7271e
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 13:18:14 +0000

    Sat, Jul 22, 2023, 4:18 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -8,18 +8,19 @@ def main():
 def get_level():
     try:
         while True:
-            levelprompt = int(input("Level: "))
-            if 3 < levelprompt or levelprompt <= 0:
+            level = int(input("Level: "))
+            if 3 < level or level <= 0:
                 pass
             else:
-                return levelprompt
+                return level
     except ValueError:
         print("Select level between 1-3")
         pass
 
 
 def generate_integer(level):
-    ...
+    for i in range(10):
+        
 
 
 if __name__ == "__main__":
