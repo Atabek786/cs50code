a960bb6313cbe062924d13dcb4df24435af7d1c7
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 21:40:57 +0000

    Sun, Jul 23, 2023, 12:40 AM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -28,18 +28,19 @@ def get_bitcoin_price():
         sys.exit(1)
 
 
-if len(sys.argv) < 2:
-    print("Missing command-line argument")
-    sys.exit(1)
-else:
-    try:
-        btc_amount = float(sys.argv[1])
-    except ValueError:
-        print("Invalid command-line argument. Please provide a valid number.")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing command-line argument")
         sys.exit(1)
+    else:
+        try:
+            btc_amount = float(sys.argv[1])
+        except ValueError:
+            print("Invalid command-line argument. Please provide a valid number.")
+            sys.exit(1)
 
-    price_usd = get_bitcoin_price()
-    in_usd = btc_amount * price_usd
-    formatted_result = format_currency(in_usd).strip()
-    print(f"${formatted_result}")
+        price_usd = get_bitcoin_price()
+        in_usd = btc_amount * price_usd
+        formatted_result = format_currency(in_usd)
+        print(f"${formatted_result}")
 
