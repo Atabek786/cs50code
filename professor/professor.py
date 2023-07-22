6c183bd06149058ce9be395eefc690caf21dc242
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 13:35:19 +0000

    Sat, Jul 22, 2023, 4:35 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -3,10 +3,15 @@ import random
 
 def main():
     level = get_level()
+    solved = 0
+    for i in range(10):
+        problem = generate_integer(level)
+        user_answer = get_user_answer()
 
-    problem = generate_integer(level)
-
-    print(problem)
+        if user_answer == problem["solution"]:
+            solved += 1
+        else:
+            print("EEE")
 
 
 def get_level():
@@ -25,8 +30,18 @@ def generate_integer(level):
     for i in range(10):
         x = random.randint(1,9)
         y = random.randint(1,9)
-        problem = f"{x} + {y} ="
+        problem = {
+            "expression": f"{x} + {y} =",
+            "solution": x + y
+        }
+
         return problem
+
+def get_user_answer():
+    try:
+        return int(input("Your answer: "))
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
 
 if __name__ == "__main__":
     main()
