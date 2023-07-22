5e42e68fe4b0dd05c63066bc3f518aafcb3a7efe
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 11:43:36 +0000

    Sat, Jul 22, 2023, 2:43 PM +03:00

--- figletpy/figlet.py
+++ figletpy/figlet.py
@@ -5,9 +5,10 @@ import random
 
 prompt = input("Input: ")
 
-if len(sys.argv) == 0 :
+if len(sys.argv) == 1 :
     converted = Figlet(font=random(prompt))
-    print(converted)
+
+    converted = 
 elif len(sys.argv) == 2 :
     converted = Figlet(font=sys.argv[2])
     print(converted)
