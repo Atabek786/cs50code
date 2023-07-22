6a42ae02895bf9e7a36f04a0e287c18c0d748ddf
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 12:47:05 +0000

    Sat, Jul 22, 2023, 3:47 PM +03:00

--- game/game.py
+++ game/game.py
@@ -5,13 +5,16 @@ randomizer = random.randint(1, 9)
 try:
     while True:
         prompt = int(input("Level: "))
-        if prompt > randomizer:
-            print("Too large")
-        elif prompt < randomizer:
-            print("Too small")
-        elif prompt == randomizer:
-            print("Just right")
-            break
+        if prompt.isdigit():
+            if prompt > randomizer:
+                print("Too large")
+            elif prompt < randomizer:
+                print("Too small")
+            elif prompt == randomizer:
+                print("Just right")
+                break
+        else:
+            pass
 except ValueError:
     pass
 
