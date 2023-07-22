7a279e6eff891d444f9bdd692dc634deaa434f5d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 12:16:17 +0000

    Sat, Jul 22, 2023, 3:16 PM +03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,7 +2,14 @@ import inflect
 
 p = inflect.engine()
 
+names = []
+
 try:
     while True:
         name = input("Name: ")
+
         
+except EOFError:
+    pass
+
+print(f"Adieu, adieu to {name}")
