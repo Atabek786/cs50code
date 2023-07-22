22f5b425e09d1f5f08da1a04359f34add4b95d0c
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 21:48:31 +0000

    Sun, Jul 23, 2023, 12:48 AM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,46 +1,24 @@
-import sys
 import requests
-import locale
+import sys
 
-url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-
-# Set the locale to use the en_US format for consistent currency formatting
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-
-
-def format_currency(amount):
-    # Format the amount to four decimal places with comma as thousands separator
-    return locale.format_string("%.4f", amount, grouping=True)
-
-
-def get_bitcoin_price():
+if len(sys.argv) == 2:
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            # The JSON response has a 'bpi' key that contains the price data in different currencies
-            price_usd = float(data["bpi"]["USD"]["rate"].replace(",", ""))
-            return price_usd
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-            sys.exit(1)
-    except requests.exceptions.RequestException as e:
-        print(f"Request error: {e}")
+        value = float(sys.argv[1])
+    except:
+        print("Command-line argument is not a number ")
         sys.exit(1)
+else:
+    print("Missing command-line argument")
+    sys.exit(1)
 
+try:
+    r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    response = r.json()
+    bitcoin_to_USD = response['bpi']['USD']['rate']
+    bitcoin_to_USD = bitcoin_to_USD.replace(",","")
+    amount = float(bitcoin_to_USD) * float(sys.argv[1])
+    print(f"${amount:,.4f}")
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Missing command-line argument")
-        sys.exit(1)
-    else:
-        try:
-            btc_amount = float(sys.argv[1])
-        except ValueError:
-            print("Invalid command-line argument. Please provide a valid number.")
-            sys.exit(1)
-
-        price_usd = get_bitcoin_price()
-        in_usd = btc_amount * price_usd
-        formatted_result = format_currency(in_usd)
-        print(formatted_result)
+except requests.RequestException:
+    print("RequestException")
+    sys.exit(1)
