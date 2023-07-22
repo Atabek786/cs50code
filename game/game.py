d4cc6e48eae833eca3c30cb656a05d26a96f2882
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 12:48:45 +0000

    Sat, Jul 22, 2023, 3:48 PM +03:00

--- game/game.py
+++ game/game.py
@@ -1,20 +1,26 @@
 import random
 
+def get_integer_input(prompt):
+    while True:
+        user_input = input(prompt)
+        if user_input.isdigit():
+            return int(user_input)
+        else:
+            print("Invalid input! Please enter a numeric value.")
+
 randomizer = random.randint(1, 9)
+
 
 try:
     while True:
-        prompt = int(input("Level: "))
-        if prompt.isdigit():
-            if prompt > randomizer:
-                print("Too large")
-            elif prompt < randomizer:
-                print("Too small")
-            elif prompt == randomizer:
-                print("Just right")
-                break
-        else:
-            pass
+        guess = get_integer_input("Guess: ")
+        if guess > randomizer:
+            print("Too large!")
+        elif guess < randomizer:
+            print("Too small!")
+        elif guess == randomizer:
+            print("Just right!")
+            break
 except ValueError:
     pass
 
