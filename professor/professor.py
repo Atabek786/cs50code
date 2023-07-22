778c2a8101e13f27f81286cf8f383e121ad2d6d1
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 13:31:07 +0000

    Sat, Jul 22, 2023, 4:31 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -19,15 +19,14 @@ def get_level():
                 return level
     except ValueError:
         print("Select level between 1-3")
-        pass
 
 
 def generate_integer(level):
     for i in range(10):
         x = random.randint(1,9)
         y = random.randint(1,9)
-        problem = x + y
-        print(problem)
+        problem = f"{x} + {y} ="
+        return problem
 
 if __name__ == "__main__":
     main()
