d3221bbe3ad1000ad9e468a04a719793e1da1568
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-22 13:16:16 +0000

    Sat, Jul 22, 2023, 4:16 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -6,7 +6,16 @@ def main():
 
 
 def get_level():
-    ...
+    try:
+        while True:
+            levelprompt = int(input("Level: "))
+            if 3 < levelprompt or levelprompt <= 0:
+                pass
+            else:
+                return levelprompt
+    except ValueError:
+        print("Select level between 1-3")
+        pass
 
 
 def generate_integer(level):
