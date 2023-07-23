6b26c023421d71874d4491279f75644a9bb38527
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 16:33:59 +0000

    Sun, Jul 23, 2023, 7:33 PM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,17 @@
 from plates import is_valid
 
-def 
+def should_be_valid():
+    assert is_valid("CS50") == True
+    assert is_valid("ATA23") == True
+
+def shouldnt_be_valid():
+    assert is_valid("50CS") == False
+    assert is_valid("23ATA") == False
+
+def should_be_valid_reverse():
+    assert is_valid("CS50") == False
+    assert is_valid("ATA23") == False
+
+def shouldnt_be_valid_reverse():
+    assert is_valid("50CS") == True
+    assert is_valid("23ATA") == True
