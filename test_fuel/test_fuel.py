f8d835d9376907ccc52cfe4ecf5a30181fadc08c
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 17:21:09 +0000

    Sun, Jul 23, 2023, 8:21 PM +03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -3,3 +3,27 @@ from fuel import convert, gauge
 def main():
     test_zero()
     test_punctuation()
+    test_trues()
+    test_letters()
+
+def test_zero():
+    assert convert("1/0") == 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
