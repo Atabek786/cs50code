7fab4b02ddfdcd085f0ecefed74fd3fa776f931d
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 15:42:30 +0000

    Sun, Jul 23, 2023, 6:42 PM +03:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,7 @@
 from twttr import shorten
 
 def test_lowercase():
-    assert shorten("my name is atabek") == "my nm s tbk"
+    assert shorten("my, name is atabek 6") == "my, nm s tbk 6"
 
 def test_uppercase():
-    assert shorten("MY NAME IS ATABEK") == "MY NM S TBK"
+    assert shorten("MY, NAME IS ATABEK 6") == "MY, NM S TBK 6"
