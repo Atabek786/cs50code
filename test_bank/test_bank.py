7b137a7c4a3bd8471300f10e33629a4e83828003
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 16:25:04 +0000

    Sun, Jul 23, 2023, 7:25 PM +03:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,20 +1,20 @@
 from bank import value
 
 def test_uppercase():
-    assert value("      HELLO, I WANT TO TAKE 100 BUCKS") == "$0"
+    assert value("      HELLO, I WANT TO TAKE 100 BUCKS") == 0
 
 
 def test_lowercase():
-    assert value("hello, i want to take 100 bucks") == "$0"
+    assert value("hello, i want to take 100 bucks") == 0
 
 
 def test_hey_uppcase():
-    assert value("HEY, I WANT TO TAKE 100 BUCKS") == "$20"
+    assert value("HEY, I WANT TO TAKE 100 BUCKS") == 20
 
 
 def test_hey_lwrcase():
-    assert value("  hey, i want to take 100 bucks") == "$20"
+    assert value("  hey, i want to take 100 bucks") == 20
 
 
 def test_other():
-    assert value("wassup, i want to take 100 bucks") == "$100"
+    assert value("wassup, i want to take 100 bucks") == 100
