056fe77e20c900ee76f3fc5adbf4f8d39422fc09
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 17:12:42 +0000

    Sun, Jul 23, 2023, 8:12 PM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,34 +1,41 @@
 from plates import is_valid
 
 def main():
-    test_length()
-    test_number()
-    test_lwrcase_and_uppcase()
-    test_long()
+    test_min_max_characters()
+    test_starting_with_two_letters()
+    test_numbers_in_middle()
+    test_first_number_zero()
+    test_other_characters()
 
-def test_length():
-    assert is_valid("C") == False
-    assert is_valid("CS") == True
-    assert is_valid("CS5") == True
+# Plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters
+def test_min_max_characters():
+    assert is_valid('AB') == True
+    assert is_valid('ABCDEF') == True
+    assert is_valid('A') == False
+    assert is_valid('ABCDEFGH') == False
+
+# Plates must start with at least two letters
+def test_starting_with_two_letters():
+    assert is_valid('AA') == True
+    assert is_valid('A2') == False
+    assert is_valid('22') == False
+
+# Numbers cannot be used in the middle of a plate; they must come at the end
+def test_numbers_in_middle():
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+
+# The first number used cannot be a ‘0’
+def test_first_number_zero():
+    assert is_valid("CS05") == False
     assert is_valid("CS50") == True
+    assert is_valid("CS50P") == False
 
-def test_number():
-    assert is_valid("5") == False
-    assert is_valid("50") == False
-    assert is_valid("50C") == False
-    assert is_valid("50CS") == False
-
-def test_lwrcase_and_uppcase():
-    assert is_valid("cS50") == True
-    assert is_valid("Cs50") == True
-    assert is_valid("cs50") == True
-    assert is_valid("CS50") == True
-
-def test_long():
-    assert is_valid("cSx50") == True
-    assert is_valid("ATA295") == True
-    assert is_valid("139Ata") == False
-    assert is_valid("A22bek") == False
+# No periods, spaces, or punctuation marks are allowed
+def test_other_characters():
+    assert is_valid("PI3.14") == False
+    assert is_valid("CS 50") == False
+    assert is_valid("Hello!") == False
 
 if __name__ == "__main__":
     main()
