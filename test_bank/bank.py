c971b49f6e2cf4cc92af3c567477ff10cfe5a114
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 16:24:09 +0000

    Sun, Jul 23, 2023, 7:24 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -2,16 +2,16 @@
 def main():
     user_greeting = input("Enter your greeting: ").strip().lower()
     fee = value(user_greeting)
-    print("Fee:", fee)
+    print(f"Fee: ${fee}")
 
 
 def value(greeting):  # Remove leading whitespace and convert to lowercase
     if greeting.startswith("hello"):
-        return "$0"
+        return 0
     elif greeting.startswith("h"):
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 
 if __name__ == "__main__":
