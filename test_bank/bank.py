3a538d0380f7205496a25f57ad61cff948eff840
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 15:53:50 +0000

    Sun, Jul 23, 2023, 6:53 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -6,7 +6,7 @@ def main():
 
 
 def value(greeting):
-    greeting = greeting.lstrip().lower()  # Remove leading whitespace and convert to lowercase
+    greeting = greeting.lstrip()  # Remove leading whitespace and convert to lowercase
 
     if greeting.startswith("hello"):
         return "$0"
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,20 +1,20 @@
 from bank import value
 
 def test_uppercase():
-    assert value("HELLO, I WANT TO TAKE 100 BUCKS")
+    assert value("HELLO, I WANT TO TAKE 100 BUCKS") == "$0"
 
 
 def test_lowercase():
-    assert value("hello, i want to take 100 bucks")
+    assert value("hello, i want to take 100 bucks") == "$0"
 
 
 def test_hey_uppcase():
-    assert value("HEY, I WANT TO TAKE 100 BUCKS")
+    assert value("HEY, I WANT TO TAKE 100 BUCKS") == "$20"
 
 
 def test_hey_lwrcase():
-    assert value("hey, i want to take 100 bucks")
+    assert value("hey, i want to take 100 bucks") == "$20"
 
 
 def test_other():
-    assert value("wassup, i want to take 100 bucks")
+    assert value("wassup, i want to take 100 bucks") == "$100"
