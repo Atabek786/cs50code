4e8478746f1605b5f12460a1ac440bd96aff564f
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-23 15:37:43 +0000

    Sun, Jul 23, 2023, 6:37 PM +03:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,7 @@
 from twttr import shorten
 
 def test_lowercase():
-    shorten("atabek") == "tbk"
+    shorten("my name is atabek") == "my nm s tbk"
 
 def test_uppercase():
-    shorten("ATABEK") == "TBK"
+    shorten("MY NAME IS ATABEK") == "MY NM S TBK"
