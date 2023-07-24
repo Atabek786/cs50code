be6dc5c51a963eb3fe80e5c472237fee6640b686
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 20:53:54 +0000

    Mon, Jul 24, 2023, 11:53 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,107 +1,45 @@
 import sys
-import os
-import numpy as np
-from PIL import Image
+from os.path import splitext
+from PIL import Image, ImageOps
 
-def resize_crop_image(input_image, target_size):
-    image = Image.open(input_image)
-    target_width, target_height = target_size
-    aspect_ratio = target_width / target_height
+def main():
+    check_command_line_arg()
+    # Open the image
+    try:
+        muppet = Image.open(sys.argv[1])
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    # Open shirt
+    shirt = Image.open("shirt.png")
+    # Get the size of the shirt
+    size = shirt.size
+    # Resize muppet image to fit shirt
+    muppet = ImageOps.fit(muppet, size)
+    # Paste shirt in muppet
+    muppet.paste(shirt, shirt)
+    # Create output image
+    muppet.save(sys.argv[2])
 
-    # Resize the image while maintaining the original aspect ratio
-    if image.width / image.height > aspect_ratio:
-        new_width = int(image.height * aspect_ratio)
-        image = image.resize((new_width, target_height))
-    else:
-        new_height = int(image.width / aspect_ratio)
-        image = image.resize((target_width, new_height))
 
-    # Crop the image to the target size
-    left = (image.width - target_width) // 2
-    top = (image.height - target_height) // 2
-    right = left + target_width
-    bottom = top + target_height
-    image = image.crop((left, top, right, bottom))
+def check_command_line_arg():
+    #Check how many elements in the command line
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    file1 = splitext(sys.argv[1])
+    file2 = splitext(sys.argv[2])
+    # Check if it is a Image file
+    if check_extension(file1[1].lower()) == False or check_extension(file2[1].lower()) == False:
+        sys.exit("Not a Image file")
+    # Check if extension of both files are the same
+    if file1[1].lower() != file2[1].lower():
+        sys.exit("Input and output have different extensions")
 
-    return image
-
-def overlay_shirt(input_image, output_image, shirt_image, x_offset=0, y_offset=0):
-    input_size = input_image.size
-    shirt_size = shirt_image.size
-
-    if input_size != shirt_size:
-        shirt_image = resize_crop_image(shirt_image, input_size)
-
-    # Convert images to RGBA mode
-    input_image = input_image.convert('RGBA')
-    shirt_image = shirt_image.convert('RGBA')
-
-    # Calculate the position to overlay the shirt
-    x_offset = x_offset
-    y_offset = y_offset
-
-    # Create a new transparent image to hold the overlay
-    overlay = Image.new('RGBA', input_size, (0, 0, 0, 0))
-    overlay.paste(shirt_image, (x_offset, y_offset))
-
-    # Composite the overlay on the input image
-    result_image = Image.alpha_composite(input_image, overlay)
-
-    # Convert the result image to RGB mode if saving as JPEG
-    if determine_output_format(output_image) == 'JPEG':
-        result_image = result_image.convert('RGB')
-
-    # Save the result image to the output file
-    result_image.save(output_image, format=determine_output_format(output_image))
-
-def determine_output_format(output_image_path):
-    _, file_extension = os.path.splitext(output_image_path)
-    if file_extension.lower() == '.jpg' or file_extension.lower() == '.jpeg':
-        return 'JPEG'
-    elif file_extension.lower() == '.png':
-        return 'PNG'
-    else:
-        raise ValueError("Unsupported output image format. Please use JPG or PNG.")
-
-def compare_images(image_path1, image_path2):
-    image1 = Image.open(image_path1)
-    image2 = Image.open(image_path2)
-
-    if image1.size != image2.size:
-        return False
-
-    array1 = np.array(image1)
-    array2 = np.array(image2)
-
-    return np.array_equal(array1, array2)
+def check_extension(file):
+    if file in [".jpg", ".jpeg",".png"]:
+        return True
+    return False
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python shirt.py before1.jpg after1.jpg")
-        sys.exit(1)
-
-    input_image_path = sys.argv[1]
-    output_image_path = sys.argv[2]
-
-    try:
-        # Load the "shirt.png" image
-        shirt_image = Image.open("shirt.png")
-
-        # Set the desired uplift value (change this to uplift or lower the shirt)
-        uplift_amount = 50
-
-        # Resize, crop, and overlay the images with the desired positioning
-        overlay_shirt(resize_crop_image(input_image_path, shirt_image.size), output_image_path, shirt_image, x_offset=0, y_offset=-uplift_amount)
-
-        # Compare the output image with the expected result
-        expected_image_path = "expected_" + os.path.basename(output_image_path)
-        if compare_images(output_image_path, expected_image_path):
-            print("Image processing complete.")
-        else:
-            print("Image does not match the expected result.")
-    except FileNotFoundError:
-        print("File Not Found :(")
-    except ValueError as ve:
-        print(ve)
-    except Exception as e:
-        print("An error occurred:", e)
+    main()
