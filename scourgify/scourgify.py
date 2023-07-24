0e478c09a4811588222544f0bf6f3923908eacea
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 19:57:36 +0000

    Mon, Jul 24, 2023, 10:57 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,49 +2,30 @@ import sys
 import os
 import csv
 
-def read_file(file_path):
-    data = []
-    try:
-        with open(file_path, 'r') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                data.append(row)
-    except FileNotFoundError:
-        print("File not found")
-    return data
-def write_file(file, data):
-    try:
-        with open(file, 'w', newline='') as file:
-            fieldnames = ['first', 'last', 'house']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(data)
-    except IOError:
-        print('Error writing to file')
-if __name__ == "__main__":
-    if len(sys.argv)  < 3:
-        print("Too few command-line arguments")
-        sys.exit(1)
-    elif len(sys.argv) > 3:
-        print("Too many command-line arguments")
-        sys.exit(1)
-    elif not sys.argv[1].endswith(".csv") and sys.argv[2].endswith(".csv"):
-        print("Not a csv file")
-        sys.exit(1)
-    elif not os.path.exists(sys.argv[1]):
-        print("Could not read "+ sys.argv[1])
-        sys.exit(1)
-    else:
-        input_file = sys.argv[1]
-        output_file = 'after.csv'
-        before = read_file(input_file)
-        for row in before:
+
+def process_data(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.DictReader(infile)
+        fieldnames = ['first', 'last', 'house']
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in reader:
             full_name = row['name']
             house = row['house']
             last, first = full_name.split(', ')
             row['first'] = first
             row['last'] = last
             row.pop('name', None)
-        write_file(output_file, before)
+            writer.writerow(row)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py input.csv output.csv")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    process_data(input_file, output_file)
 
     print(f"Data edited and saved to {output_file}")
