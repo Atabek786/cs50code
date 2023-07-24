ac8171c45a6c6c31ee6699b6b231d1b1dcd898c0
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 20:38:57 +0000

    Mon, Jul 24, 2023, 11:38 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -13,8 +13,16 @@ def overlay_shirt(input_image, output_image, shirt_image):
     if input_size != shirt_size:
         shirt_image = resize_crop_image(shirt_image, input_size)
 
+    # Convert images to RGBA mode
+    input_image = input_image.convert('RGBA')
+    shirt_image = shirt_image.convert('RGBA')
+
     # Overlay the shirt on the input image
     result_image = Image.alpha_composite(input_image, shirt_image)
+
+    # Convert the result image to RGB mode if saving as JPEG
+    if determine_output_format(output_image) == 'JPEG':
+        result_image = result_image.convert('RGB')
 
     # Save the result image to the output file
     result_image.save(output_image, format=determine_output_format(output_image))
@@ -30,7 +38,7 @@ def determine_output_format(output_image_path):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python shirt.py input_image.jpg output_image.jpg or output_image.png")
+        print("Usage: python shirt.py before1.jpg after1.jpg")
         sys.exit(1)
 
     input_image_path = sys.argv[1]
