0d2931fa81c6192ac46a2716ef6ba24eae2b567b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 20:16:09 +0000

    Mon, Jul 24, 2023, 11:16 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,8 +3,14 @@ import os
 import csv
 from PIL import Image
 
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+base_name, file_extension
+
+
 def editing_image(input_image, output_image):
-    
+
+
 
 
 
@@ -18,4 +24,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python script_name.py beforeimage.jpg afterimage.jpg or afterimage.png")
         sys.exit(1)
-    ...
+    input_image = sys.argv[1]
+    output_image = sys.argv[2]
+    editing_image(input_image, output_image)
