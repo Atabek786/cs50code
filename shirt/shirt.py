2fc5edc5d7fe6802128569d49dc85ee0f03c4ea3
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 20:20:58 +0000

    Mon, Jul 24, 2023, 11:20 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,14 +3,19 @@ import os
 import csv
 from PIL import Image
 
-input_path = sys.argv[1]
-output_path = sys.argv[2]
-inputbase_name, inputfile_extension = os.path.splitext(input_path)
-outputbase_name, inputfile_extension = os.path.splitext(input_path)
+before_image_path = sys.argv[1]
+after_image_path = sys.argv[2]
+
+before_image_base_name, before_image_extension = os.path.splitext(before_image_path)
+after_image_base_name, after_image_extension = os.path.splitext(after_image_path)
 
 
 def editing_image(input_image, output_image):
+    try:
+        image = Image.open(sys.argv[1])
 
+    except FileNotFoundError:
+        print("File Not Found :(")
 
 
 
@@ -25,6 +30,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python script_name.py beforeimage.jpg afterimage.jpg or afterimage.png")
         sys.exit(1)
+
     input_image = sys.argv[1]
     output_image = sys.argv[2]
     editing_image(input_image, output_image)
