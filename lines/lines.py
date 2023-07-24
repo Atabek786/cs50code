ce2a88bff18fda0b3d7bca624605c96ec1f2773f
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 15:31:22 +0000

    Mon, Jul 24, 2023, 6:31 PM +03:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,5 +1,12 @@
 import sys
 
 
-for arg in sys.argv[1:]:
-    
+def count_lines(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        lines_of_code = sum(1 for line in lines if line.strip() and not line.strip().startswith('#'))
+    return lines_of_code
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print()
