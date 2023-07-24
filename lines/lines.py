e63f180c2972d51fb5c8c0007c1ab239779da8a6
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 15:41:47 +0000

    Mon, Jul 24, 2023, 6:41 PM +03:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,11 +1,15 @@
 import sys
+import os
 
 
 def count_lines(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        lines_of_code = sum(1 for line in lines if line.strip() and not line.strip().startswith('#'))
-    return lines_of_code
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            lines_of_code = sum(1 for line in lines if line.strip() and not line.strip().startswith('#'))
+        return lines_of_code
+    except FileNotFoundError:
+        print("File not found")
 
 if __name__ == "__main__":
     if len(sys.argv)  < 1:
@@ -17,4 +21,9 @@ if __name__ == "__main__":
     elif not sys.argv[1].endswith(".py"):
         print("Not a python file")
         sys.exit()
-    elif 
+    elif not os.path.exists(sys.argv[1]):
+        print("File doesn't exist")
+        sys.exit()
+    else:
+        result = count_lines(sys.argv[1])
+        print(f"Lines of code: {result}")
