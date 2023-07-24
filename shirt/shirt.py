b439798f6d77b347ebf7e42fce09a0f2f49c3187
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-24 20:08:10 +0000

    Mon, Jul 24, 2023, 11:08 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,3 +4,16 @@ import csv
 
 def editing_image(input_image, output_image):
     
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py beforeimage.jpg afterimage.jpg or afterimage.png")
+        sys.exit(1)
+    ...
