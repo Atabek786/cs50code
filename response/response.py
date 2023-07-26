477627fc24ce31fbc7b2c924c1c5abed7a583100
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 10:45:03 +0000

    Wed, Jul 26, 2023, 1:45 PM +03:00

--- response/response.py
+++ response/response.py
@@ -1,13 +1,18 @@
 import re
 
 def main():
-    print(validation(input("What's your email address? ")))
+    result = validation(input("What's your email address?"))
 
-def validation(s):
-    if re.search(r"^\w+@(\w+\.)?\w+\.edu$", s, re.IGNORECASE):
+    if result:
         print("Valid")
     else:
         print("Invalid")
+
+def validation(s):
+    if re.search(r"^\w+@(\w+\.)?\w+\.edu$", s, re.IGNORECASE):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
