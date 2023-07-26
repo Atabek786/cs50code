ac8affbd81061f371f861de3862efdfe9207e9ab
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 16:24:17 +0000

    Wed, Jul 26, 2023, 7:24 PM +03:00

--- response/response.py
+++ response/response.py
@@ -9,8 +9,10 @@ def main():
             print("Valid")
     except errors.EmptyValueError:
         print("Type your email!")
+        pass
     except errors.InvalidEmailError:
         print("Invalid")
+        pass
 
 if __name__ == "__main__":
     main()
