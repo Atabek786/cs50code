f2607c0ce2e5ed6be5f3ea38a0813a84ea4ef6dc
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 10:00:02 +0000

    Wed, Jul 26, 2023, 1:00 PM +03:00

--- response/response.py
+++ response/response.py
@@ -1,15 +1,14 @@
-import re
 from validator_collection import validators, checkers, errors
 
 def main():
     print(validation(input("What's your email address? ")))
 
 def validation(s):
-        email = validators.email(s)
-        if ValueError:
-            print("INVALID")
-        else:
-            print("Valid")
+    email = validators.email(s)
+    if ValueError:
+        print("INVALID")
+    else:
+        print("Valid")
 
 if __name__ == "__main__":
     main()
