2e5cc171d994c369c2fbe6477257c66a6b4cd2b4
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 08:36:40 +0000

    Wed, Jul 26, 2023, 11:36 AM +03:00

--- working/working.py
+++ working/working.py
@@ -1,29 +1,40 @@
 import re
-import sys
-
 
 def main():
     print(convert(input("Hours: ")))
 
 
 def convert(s):
-
-    # Check if the time is in the 12-hour format (hh:mm AM/PM)
-    if re.match(r"\b(1[0-2]|0?[1-9]):[0-5][0-9] [APap][Mm]\b", s):
-        # Convert to 24-hour format (HH:MM)
-        time_obj = datetime.datetime.strptime(s, "%I:%M %p")
-        return time_obj.strftime("%H:%M")
-
-    # Check if the time is in the 24-hour format (HH:MM)
-    elif re.match(r"\b(?:2[0-3]|[01]?[0-9]):[0-5][0-9]\b", s):
-        # Convert to 12-hour format (hh:mm AM/PM)
-        time_obj = datetime.datetime.strptime(s, "%H:%M")
-        return time_obj.strftime("%I:%M %p")
-
+    isCorrectFormat = re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$", s)
+    if isCorrectFormat:
+        pieces = isCorrectFormat.groups()
+        if int(pieces[1]) > 12 or int(pieces[5]) > 12:
+            raise ValueError
+        # Create first time in correct 24-hours format
+        first_time = new_format(pieces[1], pieces[2], pieces[3])
+        # Create second time in correct 24-hours format
+        second_time = new_format(pieces[5], pieces[6], pieces[7])
+        return first_time + " to " + second_time
     else:
-        return "Format not supported"
+        raise ValueError
 
 
+def new_format(hour, minutes, am_pm):
+    if am_pm == 'PM':
+        if int(hour) == 12:
+            new_hour = 12
+        else:
+            new_hour = int(hour) + 12
+    else:
+        if int(hour) == 12:
+            new_hour = 0
+        else:
+            new_hour = int(hour)
+    if minutes == None:
+        new_time = f"{new_hour:02}" + ":00"
+    else:
+        new_time = f"{new_hour:02}" + ":" + minutes
+    return new_time
 
 
 if __name__ == "__main__":
