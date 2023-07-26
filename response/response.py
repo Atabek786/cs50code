58e49ead176a277d41f85058608bb09e3a451fe5
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 10:47:51 +0000

    Wed, Jul 26, 2023, 1:47 PM +03:00

--- response/response.py
+++ response/response.py
@@ -1,15 +1,14 @@
-import re
+from validator_collection import validators
 
 def main():
     result = validation(input("What's your email address?"))
-
     if result:
         print("Valid")
     else:
         print("Invalid")
 
 def validation(s):
-    if re.search(r"^\w+@(\w+\.)?\w+\.edu$", s, re.IGNORECASE):
+    if validators.is_email(s):
         return True
     else:
         return False
