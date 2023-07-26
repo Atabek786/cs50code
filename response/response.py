2517d2b94e04a70c754e7262a6b830ad9637e036
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-26 10:40:59 +0000

    Wed, Jul 26, 2023, 1:40 PM +03:00

--- response/response.py
+++ response/response.py
@@ -4,11 +4,7 @@ def main():
     print(validation(input("What's your email address? ")))
 
 def validation(s):
-    email = validators.email(s)
-    if ValueError:
-        print("INVALID")
-    else:
-        print("Valid")
+    if re.search(r"^\w+@(\w+\.)?\w+\.[edu|com]$")
 
 if __name__ == "__main__":
     main()
