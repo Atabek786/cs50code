6f0a5feb37493342df75f1c763fd05bfcb73bc42
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-27 20:11:07 +0000

    Thu, Jul 27, 2023, 11:11 PM +03:00

--- cs50p-final-project/convert.py
+++ cs50p-final-project/convert.py
@@ -3,7 +3,7 @@ from base64 import b64decode
 from pathlib import Path
 
 DATA_DIR = Path.cwd() / "responses"
-JSON_FILE = DATA_DIR / "An ec-1690487915.json"
+JSON_FILE = DATA_DIR / "baby -1690488612.json"
 IMAGE_DIR = Path.cwd() / "images" / JSON_FILE.stem
 
 IMAGE_DIR.mkdir(parents=True, exist_ok=True)
