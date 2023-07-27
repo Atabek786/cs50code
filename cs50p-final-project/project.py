9b85d34d5efecd8c35b0606c853bce951fbe6e9f
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-27 19:54:50 +0000

    Thu, Jul 27, 2023, 10:54 PM +03:00

--- cs50p-final-project/project.py
+++ cs50p-final-project/project.py
@@ -1,7 +1,13 @@
 import os
+import json
+from pathlib import Path
+
 import openai
 
 userprompt = input("Type text that will transform into an image: ")
+DATA_DIR = Path.cwd() / "responses"
+
+DATA_DIR.mkdir(exist_ok=True)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -12,4 +18,8 @@ response = openai.Image.create(
     response_format="b64_json",
 )
 
-print(response["data"][0]["b64_json"][:50])
+file_name = DATA_DIR / f"{userprompt[:5]}-{response['created']}.json"
+
+with open(file_name, mode="w", encoding="utf-8") as file:
+    json.dump(response, file)
+
