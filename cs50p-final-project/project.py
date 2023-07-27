52896945e633eab84f53e0bffac91d400f3418e3
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-27 20:45:23 +0000

    Thu, Jul 27, 2023, 11:45 PM +03:00

--- cs50p-final-project/project.py
+++ cs50p-final-project/project.py
@@ -3,22 +3,25 @@ import json
 from pathlib import Path
 
 import openai
+def main():
+    userprompt = input("Type text that will transform into an image: ")
+    DATA_DIR = Path.cwd() / "responses"
 
-userprompt = input("Type text that will transform into an image: ")
-DATA_DIR = Path.cwd() / "responses"
+    DATA_DIR.mkdir(exist_ok=True)
 
-DATA_DIR.mkdir(exist_ok=True)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Image.create(
+        prompt=userprompt,
+        n=1,
+        size="512x512",
+        response_format="b64_json",
+    )
 
-response = openai.Image.create(
-    prompt=userprompt,
-    n=1,
-    size="512x512",
-    response_format="b64_json",
-)
+    file_name = DATA_DIR / f"{userprompt[:5]}-{response['created']}.json"
 
-file_name = DATA_DIR / f"{userprompt[:5]}-{response['created']}.json"
+    with open(file_name, mode="w", encoding="utf-8") as file:
+        json.dump(response, file)
 
-with open(file_name, mode="w", encoding="utf-8") as file:
-    json.dump(response, file)
+if __name__ == "__main__":
+    main()
