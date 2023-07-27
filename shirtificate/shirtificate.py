5cdfe5ab9a38e8832b40eb1588a3f8fb2208ae74
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-27 16:27:21 +0000

    Thu, Jul 27, 2023, 7:27 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,34 +1,22 @@
 from fpdf import FPDF
 
-class PDF(FPDF):
+class Shirtificate(FPDF):
     def header(self):
-        # Rendering logo:
-        self.image("/workspaces/134886326/shirtificate/shirtificate.png", 10, 8, 33)
-        # Setting font: helvetica bold 15
-        self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
-        # Performing a line break:
-        self.ln(20)
+        self.set_font("helvetica", style="", size=48)
+        self.cell(w=0, h=50, txt="CS50 Shirtificate", align="C")
+        self.ln(10)
 
-    def footer(self, name):  # Accept the 'name' parameter here
-        # Position cursor at 1.5 cm from the bottom:
-        self.set_y(-15)
-        # Setting font: helvetica italic 8
-        self.set_font("helvetica", "I", 8)
-        # Printing page number:
-        self.cell(0, 10, f"{name} took CS50", align="C")
+    def image(self):
+        self.image("./shirtificate.png", 0, 60)
 
-name = input("Name: ")
-pdf = PDF()
+    def footer(self, name):
+        self.set_font(family="helvetica", style="",size=20)
+        self.set_text_color(255,255,255)
+        self.cell(w=0, h=220, txt=f"{name} took CS50", align="C")
+
+pdf = Shirtificate()
 pdf.add_page()
+pdf.image()
+pdf.shirt_text(input("Name: "))
+pdf.outpud("shirtificate.pdf")
 
-# Call the header method
-pdf.header()
-
-# Call the footer method and pass the 'name' variable
-pdf.footer(name)
-
-pdf.output("certificate.png")
