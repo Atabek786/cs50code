4d6a969bee8c961214fab35b8d6ddff557301c20
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-27 20:16:51 +0000

    Thu, Jul 27, 2023, 11:16 PM +03:00

--- cs50p-final-project/convert.py
+++ cs50p-final-project/convert.py
@@ -3,7 +3,7 @@ from base64 import b64decode
 from pathlib import Path
 
 DATA_DIR = Path.cwd() / "responses"
-JSON_FILE = DATA_DIR / "old c-1690487989.json"
+JSON_FILE = DATA_DIR / "Turki-1690488960.json"
 IMAGE_DIR = Path.cwd() / "images" / JSON_FILE.stem
 
 IMAGE_DIR.mkdir(parents=True, exist_ok=True)
