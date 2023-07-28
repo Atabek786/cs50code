3281103e0ee60cf42b10f2e171dbf375e8eb5a38
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-28 23:51:00 +0000

    Sat, Jul 29, 2023, 2:51 AM +03:00

--- cs50p/test_project.py
+++ cs50p/test_project.py
@@ -1,16 +1,38 @@
 import unittest
 from unittest.mock import patch
-from project import sports
+import random
 
-class TestSports(unittest.TestCase):
-    def test_sports_valid_choice(self):
-        # Test case 1: Valid sport type choice
-        user_input = "1\n"  # Simulate the user entering '1' followed by Enter
+# Assuming you have 'responses' defined somewhere, for example:
+responses = ["response1", "response2", "response3"]
 
-        # Patch the input function to return user_input
-        with patch('builtins.input', side_effect=user_input):
-            expected_output = "Enjoy your Active sports activities! Here are some options:\n- Baseball\n- Football\n- Soccer\n- Skiing\n- Boxing\n"
-            self.assertEqual(sports(), expected_output)
+# Import the function you want to test here if it's in a separate file.
 
-if __name__ == "__main__":
+class TestOptionsFunction(unittest.TestCase):
+
+    @patch('builtins.input', side_effect=['2'])  # Mocking user input '2'
+    def test_random_option(self, mock_input):
+        # Ensure that the function returns None (as it prints random response)
+        self.assertIsNone(options())
+
+    @patch('builtins.input', side_effect=['1', '5'])  # Mocking user input '1', '5'
+    def test_valid_option(self, mock_input):
+        # Ensure that the function returns 5 (selected valid option)
+        self.assertEqual(options(), 5)
+
+    @patch('builtins.input', side_effect=['1', '6', '2'])  # Mocking user input '1', '6', '2'
+    def test_invalid_option_then_valid(self, mock_input):
+        # Ensure that the function returns 2 (selected valid option after invalid input)
+        self.assertEqual(options(), 2)
+
+    @patch('builtins.input', side_effect=['a', '1', '3'])  # Mocking user input 'a', '1', '3'
+    def test_invalid_then_valid_with_symbol(self, mock_input):
+        # Ensure that the function returns 3 (selected valid option after invalid input with symbol)
+        self.assertEqual(options(), 3)
+
+    @patch('builtins.input', side_effect=['1', 'invalid', '4'])  # Mocking user input '1', 'invalid', '4'
+    def test_invalid_then_valid_with_exception(self, mock_input):
+        # Ensure that the function returns 4 (selected valid option after invalid input with an exception)
+        self.assertEqual(options(), 4)
+
+if __name__ == '__main__':
     unittest.main()
