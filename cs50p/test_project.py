3c6f105ec812dd14bfd9a26a7a0ae5925514a95b
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-28 23:46:51 +0000

    Sat, Jul 29, 2023, 2:46 AM +03:00

--- cs50p/test_project.py
+++ cs50p/test_project.py
@@ -1,42 +1,16 @@
-from project import options, study, sports, books, game, cook
+import unittest
+from unittest.mock import patch
+from project import sports
 
-def test_options():
-    # Test case 1: User selects an activity
-    user_input = "1\n"  # Simulate the user entering '1' followed by Enter
-    assert options(user_input) == 1
+class TestSports(unittest.TestCase):
+    def test_sports_valid_choice(self):
+        # Test case 1: Valid sport type choice
+        user_input = "1\n"  # Simulate the user entering '1' followed by Enter
 
-    # Test case 2: User doesn't know what to do
-    user_input = "something\n"  # Simulate the user entering an invalid input
-    assert options(user_input) == 1
-
-def test_study():
-    # Test case 1: Valid subject choice
-    user_input = "1\n"  # Simulate the user entering '1' followed by Enter
-    assert study(user_input) == "Great choice! Here are some topics in Math:\n- Algebra\n- Calculus\n- Geometry\n- Statistics\n"
-
-    # Test case 2: Invalid subject choice
-    user_input = "10\n"  # Simulate the user entering an invalid input
-    assert study(user_input) == "Invalid choice. Please try again."
-
-def test_sports():
-    user_input = "1\n"
-    # Monkey-patching input() to return the user_input
-    original_input_function = __builtins__.input
-    __builtins__.input = lambda _: user_input
-    try:
-        assert sports() == "Enjoy your Active sports activities! Here are some options:\n- Baseball\n- Football\n- Soccer\n- Skiing\n- Boxing\n"
-    finally:
-        # Restore the original input() function
-        __builtins__.input = original_input_function
-
-# Add similar test functions for the remaining functions (books, game, cook)
-
-# Run all the test functions
-def run_tests():
-    test_options()
-    test_study()
-    test_sports()
-    # Call the remaining test functions here (books, game, cook)
+        # Patch the input function to return user_input
+        with patch('builtins.input', side_effect=user_input):
+            expected_output = "Enjoy your Active sports activities! Here are some options:\n- Baseball\n- Football\n- Soccer\n- Skiing\n- Boxing\n"
+            self.assertEqual(sports(), expected_output)
 
 if __name__ == "__main__":
-    run_tests()
+    unittest.main()
