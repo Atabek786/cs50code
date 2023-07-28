60afbe81e176273b21f896a3b94f9242c63ad6da
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-28 22:46:35 +0000

    Sat, Jul 29, 2023, 1:46 AM +03:00

--- cs50p/project.py
+++ cs50p/project.py
@@ -1,13 +1,21 @@
 import art
+import random
+
+responses = ['Oookay, good luck doing that ! ', 'Good choice, bye!', 'Gotchaaa', '']
+
 
 def main():
+    while True:
+        print(art.text2art("What do you wanna do today?", font="ogre", chr_ignore=True))
+        option = options()
 
-        print(art.text2art("What do you wanna do today?", font="lean", chr_ignore=True))
 
+def options():
+    try:
+        n = input("P.S if you don't know what to do, press 1")
+        if n != '1' or n != 1:
+            print("Oookay, good luck doing that")
 
-
-def function_1():
-    ...
 
 
 def function_2():
