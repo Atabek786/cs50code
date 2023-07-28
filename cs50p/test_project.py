18b00f30ed6021173fa3fcd17160f3767e794921
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-28 23:52:31 +0000

    Sat, Jul 29, 2023, 2:52 AM +03:00

--- cs50p/test_project.py
+++ cs50p/test_project.py
@@ -1,38 +1,32 @@
-import unittest
-from unittest.mock import patch
 import random
+from project import options
 
 # Assuming you have 'responses' defined somewhere, for example:
 responses = ["response1", "response2", "response3"]
 
-# Import the function you want to test here if it's in a separate file.
+def test_random_option(monkeypatch):
+    # Mocking user input '2'
+    monkeypatch.setattr('builtins.input', lambda _: '2')
+    # Ensure that the function returns None (as it prints random response)
+    assert options() is None
 
-class TestOptionsFunction(unittest.TestCase):
+def test_valid_option(monkeypatch):
+    # Mocking user input '1', '5'
+    monkeypatch.setattr('builtins.input', lambda _: '1')
+    assert options() == 5
 
-    @patch('builtins.input', side_effect=['2'])  # Mocking user input '2'
-    def test_random_option(self, mock_input):
-        # Ensure that the function returns None (as it prints random response)
-        self.assertIsNone(options())
+def test_invalid_option_then_valid(monkeypatch):
+    # Mocking user input '1', '6', '2'
+    monkeypatch.setattr('builtins.input', lambda _: '1', '6', '2')
+    assert options() == 2
 
-    @patch('builtins.input', side_effect=['1', '5'])  # Mocking user input '1', '5'
-    def test_valid_option(self, mock_input):
-        # Ensure that the function returns 5 (selected valid option)
-        self.assertEqual(options(), 5)
+def test_invalid_then_valid_with_symbol(monkeypatch):
+    # Mocking user input 'a', '1', '3'
+    monkeypatch.setattr('builtins.input', lambda _: 'a', '1', '3')
+    assert options() == 3
 
-    @patch('builtins.input', side_effect=['1', '6', '2'])  # Mocking user input '1', '6', '2'
-    def test_invalid_option_then_valid(self, mock_input):
-        # Ensure that the function returns 2 (selected valid option after invalid input)
-        self.assertEqual(options(), 2)
+def test_invalid_then_valid_with_exception(monkeypatch):
+    # Mocking user input '1', 'invalid', '4'
+    monkeypatch.setattr('builtins.input', lambda _: '1', 'invalid', '4')
+    assert options() == 4
 
-    @patch('builtins.input', side_effect=['a', '1', '3'])  # Mocking user input 'a', '1', '3'
-    def test_invalid_then_valid_with_symbol(self, mock_input):
-        # Ensure that the function returns 3 (selected valid option after invalid input with symbol)
-        self.assertEqual(options(), 3)
-
-    @patch('builtins.input', side_effect=['1', 'invalid', '4'])  # Mocking user input '1', 'invalid', '4'
-    def test_invalid_then_valid_with_exception(self, mock_input):
-        # Ensure that the function returns 4 (selected valid option after invalid input with an exception)
-        self.assertEqual(options(), 4)
-
-if __name__ == '__main__':
-    unittest.main()
