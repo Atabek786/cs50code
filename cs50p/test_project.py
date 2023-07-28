3e0c1c433f7b4d59c6df809e3a9b9ed7bfbba972
Atabek <134886326+Atabek2005@users.noreply.github.com>
2023-07-28 23:55:14 +0000

    Sat, Jul 29, 2023, 2:55 AM +03:00

--- cs50p/test_project.py
+++ cs50p/test_project.py
@@ -5,28 +5,27 @@ from project import options
 responses = ["response1", "response2", "response3"]
 
 def test_random_option(monkeypatch):
-    # Mocking user input '1' (to take the else branch)
-    monkeypatch.setattr('builtins.input', lambda _: '1')
+    # Mocking user input '2' (to take the 'else' branch)
+    monkeypatch.setattr('builtins.input', lambda _: '2')
     # Ensure that the function returns None (as it prints random response)
     assert options() is None
 
 def test_valid_option(monkeypatch):
-    # Mocking user input '1', '2'
-    monkeypatch.setattr('builtins.input', lambda _: '1', '2')
-    assert options() == 2
+    # Mocking user input '1' (to take the 'if' branch)
+    monkeypatch.setattr('builtins.input', lambda _: '1')
+    assert options() == 1
 
 def test_invalid_option_then_valid(monkeypatch):
-    # Mocking user input '1', '6', '2'
+    # Mocking user input '1', '2' (to take the 'if' branch)
     monkeypatch.setattr('builtins.input', lambda _: '1', '2')
     assert options() == 2
 
 def test_invalid_then_valid_with_symbol(monkeypatch):
-    # Mocking user input '1', '3'
+    # Mocking user input '1', '3' (to take the 'if' branch)
     monkeypatch.setattr('builtins.input', lambda _: '1', '3')
     assert options() == 3
 
 def test_invalid_then_valid_with_exception(monkeypatch):
-    # Mocking user input '1', '4'
+    # Mocking user input '1', '4' (to take the 'if' branch)
     monkeypatch.setattr('builtins.input', lambda _: '1', '4')
     assert options() == 4
-
